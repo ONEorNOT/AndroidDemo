4326b12a05311b4ab753fbdf95503e632cb9cbc8
sealiu <sealiu0217@gmail.com>
2016-04-26 00:48:42 +0800

    大作业/Convert-界面的部分绘制

---
+++ sealiu/Convert/app/src/main/java/com/sealiu/convert/Currency.java
@@ -0,0 +1,15 @@
+package com.sealiu.convert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Currency extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
---
+++ sealiu/Convert/app/src/main/java/com/sealiu/convert/MainActivity.java
@@ -0,0 +1,48 @@
+package com.sealiu.convert;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView linearLayout_currency = (TextView) findViewById(R.id.currency_convert);
+        linearLayout_currency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Currency.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_about:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
